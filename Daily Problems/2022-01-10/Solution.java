017c2919fd04722d995f33f2cefc3e851076ce21
Pei-Chen <pechen1202@gmail.com>
2022-01-10 22:12:41 +1100

    Create Solution.java

---
+++ Daily Problems/2022-01-10/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int alen = a.length()-1;
+        int blen = b.length()-1;
+        int carry=0;
+        StringBuilder sb = new StringBuilder();
+        while(alen>=0 || blen>=0) {
+            int sum = carry;
+            if(alen>=0) {
+                sum+=(a.charAt(alen--)-'0');
+            }
+            if(blen>=0) {
+                sum+=(b.charAt(blen--)-'0');
+            }
+            sb.insert(0, sum%2);
+            carry=sum>>1;
+        }
+        if(carry==1) {
+            sb.insert(0,1);
+        }
+		//return String
+        return sb.toString();
+    }
+}
