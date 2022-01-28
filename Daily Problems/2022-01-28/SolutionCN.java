43f8a887b34d520ca7b6b3a943d76d960b93d6ad
Pei-Chen <pechen1202@gmail.com>
2022-01-28 20:31:26 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-01-28/SolutionCN.java
@@ -0,0 +1,17 @@
+class SolutionCN {
+    public int numberOfWeakCharacters(int[][] properties) {
+        Arrays.sort(properties, (o1, o2) -> {
+            return o1[0] == o2[0] ? (o1[1] - o2[1]) : (o2[0] - o1[0]);
+        });
+        int maxDef = 0;
+        int ans = 0;
+        for (int[] p : properties) {
+            if (p[1] < maxDef) {
+                ans++;
+            } else {
+                maxDef = p[1];
+            }
+        }
+        return ans;
+    }
+}
