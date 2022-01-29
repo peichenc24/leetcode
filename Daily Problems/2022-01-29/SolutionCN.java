35a659c56bdc444302f61b4f7468b351b007259d
Pei-Chen <pechen1202@gmail.com>
2022-01-29 20:33:22 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-01-29/SolutionCN.java
@@ -0,0 +1,31 @@
+class SolutionCN {
+    int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length, n = isWater[0].length;
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            Arrays.fill(ans[i], -1);
+        }
+        Queue<int[]> queue = new ArrayDeque<int[]>();
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isWater[i][j] == 1) {
+                    ans[i][j] = 0;
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] p = queue.poll();
+            for (int[] dir : dirs) {
+                int x = p[0] + dir[0], y = p[1] + dir[1];
+                if (0 <= x && x < m && 0 <= y && y < n && ans[x][y] == -1) {
+                    ans[x][y] = ans[p[0]][p[1]] + 1;
+                    queue.offer(new int[]{x, y});
+                }
+            }
+        }
+        return ans;
+    }
+}
