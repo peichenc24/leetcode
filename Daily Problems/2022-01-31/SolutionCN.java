6edc456671d9626cb3e12587bd0f175d83a33d4e
Pei-Chen <pechen1202@gmail.com>
2022-01-31 23:11:38 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-01-31/SolutionCN.java
@@ -0,0 +1,10 @@
+class SolutionCN {
+    public int numberOfSteps(int num) {
+        int ret = 0;
+        while (num > 0) {
+            ret += (num > 1 ? 1 : 0) + (num & 0x01);
+            num >>= 1;
+        }
+        return ret;
+    }
+}
