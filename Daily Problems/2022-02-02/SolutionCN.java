a90a8d5da48204709d3315dbcafadde5ed94a7eb
Pei-Chen <pechen1202@gmail.com>
2022-02-02 22:28:01 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-02-02/SolutionCN.java
@@ -0,0 +1,18 @@
+class SolutionCN {
+    public String reversePrefix(String word, char ch) {
+        int index = word.indexOf(ch);
+        if (index >= 0) {
+            char[] arr = word.toCharArray();
+            int left = 0, right = index;
+            while (left < right) {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            word = new String(arr);
+        }
+        return word;
+    }
+}
