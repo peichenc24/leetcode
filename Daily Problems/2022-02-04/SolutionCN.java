d9ba074aeb06b1624a49ef1c91fb52810c826642
Pei-Chen <pechen1202@gmail.com>
2022-02-04 21:36:21 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-02-04/SolutionCN.java
@@ -0,0 +1,16 @@
+class SolutionCN {
+    public int countGoodRectangles(int[][] rectangles) {
+        int res = 0, maxLen = 0;
+        for (int[] rectangle : rectangles) {
+            int l = rectangle[0], w = rectangle[1];
+            int k = Math.min(l, w);
+            if (k == maxLen) {
+                ++res;
+            } else if (k > maxLen) {
+                res = 1;
+                maxLen = k;
+            }
+        }
+        return res;
+    }
+}
