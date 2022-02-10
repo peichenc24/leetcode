2a936981701aa3e4dc3f8eeb2c0de121012e56d9
Pei-Chen <pechen1202@gmail.com>
2022-02-10 23:28:08 +1100

    Update Solution.java

--- Daily Problems/2022-02-10/Solution.java
+++ Daily Problems/2022-02-10/Solution.java
@@ -1,13 +1,15 @@
 class Solution {
-    public int findPairs(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap();
- 			for (int num : nums)
- 				map.put(num, map.getOrDefault(num, 0) + 1);
-
- 			int result = 0;
- 			for (int i : map.keySet())
- 				if (k > 0 && map.containsKey(i + k) || k == 0 && map.get(i) > 1)
- 					result++;
- 			return result;
+    public int subarraySum(int[] nums, int k) {
+        int ans = 0;
+        for(int i=0;i<nums.length;i++) {
+            int sum=0;
+            for(int j=i;j<nums.length;j++) {
+                sum+=nums[j];
+                if(sum==k) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
     }
 }
