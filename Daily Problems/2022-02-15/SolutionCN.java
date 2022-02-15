1a703c5a5abd7e041115c7c18f161ad3d46bb7f0
Pei-Chen <pechen1202@gmail.com>
2022-02-15 23:18:03 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-02-15/SolutionCN.java
@@ -0,0 +1,30 @@
+class SolutionCN {
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isMin = true, isMax = true;
+                for (int k = 0; k < n; k++) {
+                    if (matrix[i][k] < matrix[i][j]) {
+                        isMin = false;
+                        break;
+                    }
+                }
+                if (!isMin) {
+                    continue;
+                }
+                for (int k = 0; k < m; k++) {
+                    if (matrix[k][j] > matrix[i][j]) {
+                        isMax = false;
+                        break;
+                    }
+                }
+                if (isMax) {
+                    ret.add(matrix[i][j]);
+                }
+            }
+        }
+        return ret;
+    }
+}
