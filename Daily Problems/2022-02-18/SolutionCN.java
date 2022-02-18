549b13a5f004383c05cc33d6a548eb6cf3e69f4b
Pei-Chen <pechen1202@gmail.com>
2022-02-18 20:34:21 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-02-18/SolutionCN.java
@@ -0,0 +1,15 @@
+class SolutionCN {
+    public int findCenter(int[][] edges) {
+        int n = edges.length + 1;
+        int[] degrees = new int[n + 1];
+        for (int[] edge : edges) {
+            degrees[edge[0]]++;
+            degrees[edge[1]]++;
+        }
+        for (int i = 1; ; i++) {
+            if (degrees[i] == n - 1) {
+                return i;
+            }
+        }
+    }
+}
