af913bee88fe363fd1ba225243f5badb4d2fd39f
Pei-Chen <pechen1202@gmail.com>
2022-02-24 01:00:53 +1100

    Create SolutionCN.java

---
+++ Daily Problems/2022-02-23/SolutionCN.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int left = 0, right = n - 1;
+        while (true) {
+            while (left < right && !Character.isLetter(s.charAt(left))) { // 判断左边是否扫描到字母
+                left++;
+            }
+            while (right > left && !Character.isLetter(s.charAt(right))) { // 判断右边是否扫描到字母
+                right--;
+            }
+            if (left >= right) {
+                break;
+            }
+            swap(arr, left, right);
+            left++;
+            right--;
+        }
+        return new String(arr);
+    }
+
+    public void swap(char[] arr, int left, int right) {
+        char temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}
