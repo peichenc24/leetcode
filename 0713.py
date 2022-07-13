9ba992586bb2b2acd3eed7df887aeb55fa31bdc0
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-07-13 22:22:00 +0800

    Create 0713.py

---
+++ 0713.py
@@ -0,0 +1,12 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        st = []
+        for aster in asteroids:
+            alive = True
+            while alive and aster < 0 and st and st[-1] > 0:
+                alive = st[-1] < -aster
+                if st[-1] <= -aster:
+                    st.pop()
+            if alive:
+                st.append(aster)
+        return st
