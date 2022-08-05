70225085903e82662f40106078648213a16eefde
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-08-05 22:14:30 +0800

    Create 0805.java

---
+++ 0805.java
@@ -0,0 +1,18 @@
+class Solution {
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if (root == null) {
+            return null;
+        }
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
+        }
+        if (depth == 2) {
+            root.left = new TreeNode(val, root.left, null);
+            root.right = new TreeNode(val, null, root.right);
+        } else {
+            root.left = addOneRow(root.left, val, depth - 1);
+            root.right = addOneRow(root.right, val, depth - 1);
+        }
+        return root;
+    }
+}
