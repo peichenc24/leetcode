e2d432cf7dea726f98264c156d65d827e505e45f
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-08-16 23:34:36 +0800

    Create 0816.java

---
+++ 0816.java
@@ -0,0 +1,19 @@
+class OrderedStream {
+    private String[] stream;
+    private int ptr;
+
+    public OrderedStream(int n) {
+        stream = new String[n + 1];
+        ptr = 1;
+    }
+
+    public List<String> insert(int idKey, String value) {
+        stream[idKey] = value;
+        List<String> res = new ArrayList<String>();
+        while (ptr < stream.length && stream[ptr] != null) {
+            res.add(stream[ptr]);
+            ++ptr;
+        }
+        return res;
+    }
+}
