3dd686da4e4c6fcd12bfc304f3a8a5a756746f33
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-08-21 21:53:38 +0800

    Create 0821.java

---
+++ 0821.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        int n = sentence.length(), index = 1, start = 0, end = 0;
+        while (start < n) {
+            while (end < n && sentence.charAt(end) != ' ') {
+                end++;
+            }
+            if (isPrefix(sentence, start, end, searchWord)) {
+                return index;
+            }
+
+            index++;
+            end++;
+            start = end;
+        }
+        return -1;
+    }
+
+    public boolean isPrefix(String sentence, int start, int end, String searchWord) {
+        for (int i = 0; i < searchWord.length(); i++) {
+            if (start + i >= end || sentence.charAt(start + i) != searchWord.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
