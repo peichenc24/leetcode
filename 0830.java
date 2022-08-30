bfc159561ba7b0e6a4d9b35be16ced47898b36f9
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-08-30 23:11:30 +0800

    Create 0830.java

---
+++ 0830.java
@@ -0,0 +1,21 @@
+class Solution {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        TreeNode parent = null;
+        TreeNode cur = root;
+        while (cur != null) {
+            if (val > cur.val) {
+                if (parent == null) {
+                    return new TreeNode(val, root, null);
+                }
+                TreeNode node = new TreeNode(val, cur, null);
+                parent.right = node;
+                return root;
+            } else {
+                parent = cur;
+                cur = cur.right;
+            }
+        }
+        parent.right = new TreeNode(val);
+        return root;
+    }
+}
