01a76193e00cffbe9e35e3101425c13ca207066f
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-09-09 22:59:01 +0800

    Create 0909.java

---
+++ 0909.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int depth = 0;
+        for (String log : logs) {
+            if ("./".equals(log)) {
+                continue;
+            } else if ("../".equals(log)) {
+                if (depth > 0) {
+                    depth--;
+                }
+            } else {
+                depth++;
+            }
+        }
+        return depth;
+    }
+}
