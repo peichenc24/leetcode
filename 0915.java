abe59b4620b5dab9855251b92a2d4d2e5f0e1da2
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-09-15 22:48:12 +0800

    Create 0915.java

---
+++ 0915.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int flipLights(int n, int presses) {
+        Set<Integer> seen = new HashSet<Integer>();
+        for (int i = 0; i < 1 << 4; i++) {
+            int[] pressArr = new int[4];
+            for (int j = 0; j < 4; j++) {
+                pressArr[j] = (i >> j) & 1;
+            }
+            int sum = Arrays.stream(pressArr).sum();
+            if (sum % 2 == presses % 2 && sum <= presses) {
+                int status = pressArr[0] ^ pressArr[1] ^ pressArr[3];
+                if (n >= 2) {
+                    status |= (pressArr[0] ^ pressArr[1]) << 1;
+                }
+                if (n >= 3) {
+                    status |= (pressArr[0] ^ pressArr[2]) << 2;
+                }
+                if (n >= 4) {
+                    status |= (pressArr[0] ^ pressArr[1] ^ pressArr[3]) << 3;
+                }
+                seen.add(status);
+            }
+        }
+        return seen.size();
+    }
+}
