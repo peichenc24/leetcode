730df18b7115c5cdaf0eac667663f179fafb6e80
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-09-29 23:00:36 +0800

    Create 0929.java

---
+++ 0929.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isFlipedString(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        if (m != n) {
+            return false;
+        }
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < n; j++) {
+                if (s1.charAt((i + j) % n) != s2.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
