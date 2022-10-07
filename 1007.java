760e1b052429cf67e30c863b69ef08e8ad67dd59
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-10-07 23:17:42 +0800

    Create 1007.java

---
+++ 1007.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int res = 0;
+        int l = 0;
+        while (l < nums.length) {
+            int cursum = nums[l++];
+            while (l < nums.length && nums[l] > nums[l - 1]) {
+                cursum += nums[l++];
+            }
+            res = Math.max(res, cursum);
+        }
+        return res;
+    }
+}
