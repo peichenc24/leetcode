4a4ac252eefd29ee148677050a784a617dd2f1cc
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-10-21 22:24:21 +0800

    Create 1021.java

---
+++ 1021.java
@@ -0,0 +1,20 @@
+class StockSpanner {
+    Deque<int[]> stack;
+    int idx;
+
+    public StockSpanner() {
+        stack = new ArrayDeque<int[]>();
+        stack.push(new int[]{-1, Integer.MAX_VALUE});
+        idx = -1;
+    }
+
+    public int next(int price) {
+        idx++;
+        while (price >= stack.peek()[1]) {
+            stack.pop();
+        }
+        int ret = idx - stack.peek()[0];
+        stack.push(new int[]{idx, price});
+        return ret;
+    }
+}
