aa4bc4fc5319ac6954a902fe5d6e747a3cfcf7d6
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-10-29 23:36:55 +0800

    Create 1029.java

---
+++ 1029.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = new HashMap<String, Integer>() {{
+            put("type", 0);
+            put("color", 1);
+            put("name", 2);
+        }}.get(ruleKey);
+        int res = 0;
+        for (List<String> item : items) {
+            if (item.get(index).equals(ruleValue)) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
