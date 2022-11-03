e6df80c5d75332f6284a367f1d466fdf8e5e4327
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-11-03 23:12:29 +0800

    Create 1103.java

---
+++ 1103.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxRepeating(String sequence, String word) {
+        int n = sequence.length(), m = word.length();
+        if (n < m) {
+            return 0;
+        }
+
+        int[] f = new int[n];
+        for (int i = m - 1; i < n; ++i) {
+            boolean valid = true;
+            for (int j = 0; j < m; ++j) {
+                if (sequence.charAt(i - m + j + 1) != word.charAt(j)) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                f[i] = (i == m - 1 ? 0 : f[i - m]) + 1;
+            }
+        }
+
+        return Arrays.stream(f).max().getAsInt();
+    }
+}
