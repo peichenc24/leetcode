9f4c7d33d4515e647fb7a80f29183c302776dafe
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-11-04 23:34:03 +0800

    Create 1104.java

---
+++ 1104.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int k = 0;
+        while (target > 0) {
+            k++;
+            target -= k;
+        }
+        return target % 2 == 0 ? k : k + 1 + k % 2;
+    }
+}
