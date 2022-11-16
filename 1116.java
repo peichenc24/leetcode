c0bacaf948ce4320c8d64eba7fb0daa3e9627714
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-11-16 21:06:06 +0800

    Create 1116.java

---
+++ 1116.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isIdealPermutation(int[] nums) {
+        int n = nums.length, minSuff = nums[n - 1];
+        for (int i = n - 3; i >= 0; i--) {
+            if (nums[i] > minSuff) {
+                return false;
+            }
+            minSuff = Math.min(minSuff, nums[i + 1]);
+        }
+        return true;
+    }
+}
