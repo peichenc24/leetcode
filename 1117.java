109a4619dc3ce0892df9e5bb40c1368df5fa1132
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-11-17 22:05:34 +0800

    Create 1117.java

---
+++ 1117.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int numMatchingSubseq(String s, String[] words) {
+        List<Integer>[] pos = new List[26];
+        for (int i = 0; i < 26; ++i) {
+            pos[i] = new ArrayList<Integer>();
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            pos[s.charAt(i) - 'a'].add(i);
+        }
+        int res = words.length;
+        for (String w : words) {
+            if (w.length() > s.length()) {
+                --res;
+                continue;
+            }
+            int p = -1;
+            for (int i = 0; i < w.length(); ++i) {
+                char c = w.charAt(i);
+                if (pos[c - 'a'].isEmpty() || pos[c - 'a'].get(pos[c - 'a'].size() - 1) <= p) {
+                    --res;
+                    break;
+                }
+                p = binarySearch(pos[c - 'a'], p);
+            }
+        }
+        return res;
+    }
+
+    public int binarySearch(List<Integer> list, int target) {
+        int left = 0, right = list.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid) > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return list.get(left);
+    }
+}
