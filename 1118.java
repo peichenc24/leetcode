17a2942eac873612e6613795a04484c360fb0bca
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-11-18 21:57:28 +0800

    Create 1118.java

---
+++ 1118.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sumSubseqWidths(int[] nums) {
+        final int MOD = 1000000007;
+        Arrays.sort(nums);
+        long res = 0;
+        long x = nums[0], y = 2;
+        for (int j = 1; j < nums.length; j++) {
+            res = (res + nums[j] * (y - 1) - x) % MOD;
+            x = (x * 2 + nums[j]) % MOD;
+            y = y * 2 % MOD;
+        }
+        return (int) ((res + MOD) % MOD);
+    }
+}
