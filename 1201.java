356d35e7b7aa635121644d1e33b801bfa5586096
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-12-01 22:03:25 +0800

    Create 1201.java

---
+++ 1201.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int nearestValidPoint(int x, int y, int[][] points) {
+        int n = points.length;
+        int best = Integer.MAX_VALUE, bestid = -1;
+        for (int i = 0; i < n; ++i) {
+            int px = points[i][0], py = points[i][1];
+            if (x == px) {
+                int dist = Math.abs(y - py);
+                if (dist < best) {
+                    best = dist;
+                    bestid = i;
+                }
+            } else if (y == py) {
+                int dist = Math.abs(x - px);
+                if (dist < best) {
+                    best = dist;
+                    bestid = i;
+                }
+            }
+        }
+        return bestid;
+    }
+}
