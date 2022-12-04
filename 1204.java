04b2ae9e1afca2aa21e80598aec482702419da95
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-12-04 22:24:40 +0800

    Create 1204.java

---
+++ 1204.java
@@ -0,0 +1,29 @@
+class Solution {
+    int res;
+
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+        res = Arrays.stream(baseCosts).min().getAsInt();
+        for (int b : baseCosts) {
+            dfs(toppingCosts, 0, b, target);
+        }
+        return res;
+    }
+
+    public void dfs(int[] toppingCosts, int p, int curCost, int target) {
+        if (Math.abs(res - target) < curCost - target) {
+            return;
+        } else if (Math.abs(res - target) >= Math.abs(curCost - target)) {
+            if (Math.abs(res - target) > Math.abs(curCost - target)) {
+                res = curCost;
+            } else {
+                res = Math.min(res, curCost);
+            }
+        }
+        if (p == toppingCosts.length) {
+            return;
+        }
+        dfs(toppingCosts, p + 1, curCost + toppingCosts[p] * 2, target);
+        dfs(toppingCosts, p + 1, curCost + toppingCosts[p], target);
+        dfs(toppingCosts, p + 1, curCost, target);
+    }
+}
