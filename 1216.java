6b729b3bbd56c234e8d2e71c46571acd474bfc26
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-12-16 23:44:16 +0800

    Create 1216.java

---
+++ 1216.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int minElements(int[] nums, int limit, int goal) {
+        long sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        long diff = Math.abs(sum - goal);
+        return (int) ((diff + limit - 1) / limit);
+    }
+}
