e7a45dcc375c7c0734bc4e771864c9adf4f367c6
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2022-12-21 22:48:14 +0800

    Create 1221.java

---
+++ 1221.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int maximumScore(int a, int b, int c) {
+        int sum = a + b + c;
+        int maxVal = Math.max(Math.max(a, b), c);
+        return Math.min(sum - maxVal, sum / 2);
+    }
+}
