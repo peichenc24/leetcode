478037664cb02c177d8ef54cb1f2bb01663b59cc
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-01-06 22:17:30 +0800

    Create 20230106.java

---
+++ 20230106.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countEven(int num) {
+        int res = 0;
+        for (int i = 1; i <= num; i++) {
+            int x = i, sum = 0;
+            while (x != 0) {
+                sum += x % 10;
+                x /= 10;
+            }
+            if (sum % 2 == 0) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
