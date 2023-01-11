3bd5007eb8de3fda02b31a31203fd60862b59e73
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-01-11 23:00:22 +0800

    Create 20230111.java

---
+++ 20230111.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean digitCount(String num) {
+        Map<Integer, Integer> h = new HashMap<Integer, Integer>();
+        int n = num.length();
+        for (int i = 0; i < n; i++) {
+            int digit = num.charAt(i) - '0';
+            h.put(digit, h.getOrDefault(digit, 0) + 1);
+        }
+        for (int i = 0; i < n; i++) {
+            int v = num.charAt(i) - '0';
+            if (h.getOrDefault(i, 0) != v) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
