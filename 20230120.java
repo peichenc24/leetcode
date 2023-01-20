380376f35f1888c0aaf20331cf4b71178768ae1e
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-01-20 22:18:29 +0800

    Create 20230120.java

---
+++ 20230120.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        Map<Integer, Set<Integer>> activeMinutes = new HashMap<Integer, Set<Integer>>();
+        for (int[] log : logs) {
+            int id = log[0], time = log[1];
+            activeMinutes.putIfAbsent(id, new HashSet<Integer>());
+            activeMinutes.get(id).add(time);
+        }
+        int[] answer = new int[k];
+        for (Set<Integer> minutes : activeMinutes.values()) {
+            int activeCount = minutes.size();
+            answer[activeCount - 1]++;
+        }
+        return answer;
+    }
+}
