1308c032d3634e16d1340b255d7a17b62085be0d
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-01-23 21:27:24 +0800

    Create 20230123.java

---
+++ 20230123.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double calculateTax(int[][] brackets, int income) {
+        double totalTax = 0;
+        int lower = 0;
+        for (int[] bracket : brackets) {
+            int upper = bracket[0], percent = bracket[1];
+            int tax = (Math.min(income, upper) - lower) * percent;
+            totalTax += tax;
+            if (income <= upper) {
+                break;
+            }
+            lower = upper;
+        }
+        return (double) totalTax / 100.0;
+    }
+}
