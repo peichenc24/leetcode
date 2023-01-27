572f166ab3098607acd1cecaaa3c46703daeabda
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-01-27 22:46:45 +0800

    Create 20230127.java

---
+++ 20230127.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String greatestLetter(String s) {
+        Set<Character> ht = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            ht.add(c);
+        }
+        for (int i = 25; i >= 0; i--) {
+            if (ht.contains((char) ('a' + i)) && ht.contains((char) ('A' + i))) {
+                return String.valueOf((char) ('A' + i));
+            }
+        }
+        return "";
+    }
+}
