2462029617e91edf1251498c6909c8a0162fcc29
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-01-29 22:07:28 +0800

    Create 20230129.java

---
+++ 20230129.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countAsterisks(String s) {
+        boolean valid = true;
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '|') {
+                valid = !valid;
+            } else if (c == '*' && valid) {
+                res++;
+            }
+        }
+        return res;
+    }
+}
