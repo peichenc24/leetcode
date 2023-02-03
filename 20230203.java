5b138b75aabb42e9c33edee2872d01b46a3778be
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-02-03 22:35:30 +0800

    Create 20230203.java

---
+++ 20230203.java
@@ -0,0 +1,36 @@
+class Solution {
+    TreeNode xNode;
+
+    public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
+        find(root, x);
+        int leftSize = getSubtreeSize(xNode.left);
+        if (leftSize >= (n + 1) / 2) {
+            return true;
+        }
+        int rightSize = getSubtreeSize(xNode.right);
+        if (rightSize >= (n + 1) / 2) {
+            return true;
+        }
+        int remain = n - 1 - leftSize - rightSize;
+        return remain >= (n + 1) / 2;
+    }
+
+    public void find(TreeNode node, int x) {
+        if (xNode != null || node == null) {
+            return;
+        }
+        if (node.val == x) {
+            xNode = node;
+            return;
+        }
+        find(node.left, x);
+        find(node.right, x);
+    }
+
+    public int getSubtreeSize(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + getSubtreeSize(node.left) + getSubtreeSize(node.right);
+    }
+}
