29b2d787de89c817cfdda407707f1324deae2bf6
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-02-06 23:17:34 +0800

    Create 20230206.java

---
+++ 20230206.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean evaluateTree(TreeNode root) {
+        if (root.left == null) {
+            return root.val == 1;
+        } 
+        if (root.val == 2) {
+            return evaluateTree(root.left) || evaluateTree(root.right);
+        } else {
+            return evaluateTree(root.left) && evaluateTree(root.right);
+        }
+    }
+}
