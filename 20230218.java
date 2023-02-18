1c84ea6d5d4240bbf7ea857786577e873ced6f55
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-02-18 21:30:28 +0800

    Create 20230218.java

---
+++ 20230218.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<Integer>> findSolution(CustomFunction customfunction, int z) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        for (int x = 1; x <= 1000; x++) {
+            for (int y = 1; y <= 1000; y++) {
+                if (customfunction.f(x, y) == z) {
+                    List<Integer> pair = new ArrayList<Integer>();
+                    pair.add(x);
+                    pair.add(y);
+                    res.add(pair);
+                }
+            }
+        }
+        return res;
+    }
+}
