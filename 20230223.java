29102e2d4fd2e9eacb27f1e2a141bde4ad9a9f3b
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-02-23 22:03:02 +0800

    Create 20230223.java

---
+++ 20230223.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<Integer> circularPermutation(int n, int start) {
+        List<Integer> ret = new ArrayList<Integer>();
+        ret.add(start);
+        for (int i = 1; i <= n; i++) {
+            int m = ret.size();
+            for (int j = m - 1; j >= 0; j--) {
+                ret.add(((ret.get(j) ^ start) | (1 << (i - 1))) ^ start);
+            }
+        }
+        return ret;
+    }
+}
