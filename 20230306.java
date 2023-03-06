89cea15245431a77ef3a6ab7a58f870fd004ea54
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-03-06 23:08:22 +0800

    Create 20230306.java

---
+++ 20230306.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minimumDeletions(String s) {
+        int leftb = 0, righta = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'a') {
+                righta++;
+            }
+        }
+        int res = righta;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a') {
+                righta--;
+            } else {
+                leftb++;
+            }
+            res = Math.min(res, leftb + righta);
+        }
+        return res;
+    }
+}
