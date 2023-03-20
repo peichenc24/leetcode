1b34d425ca65e81502838e9ff5e9953c3fa7811c
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-03-20 23:10:58 +0800

    Create 20230320.java

---
+++ 20230320.java
@@ -0,0 +1,32 @@
+class Solution {
+    int[][] dp;
+
+    public int numDupDigitsAtMostN(int n) {
+        String sn = String.valueOf(n);
+        dp = new int[sn.length()][1 << 10];
+        for (int i = 0; i < sn.length(); i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return n + 1 - f(0, sn, 0, true);
+    }
+
+    public int f(int mask, String sn, int i, boolean same) {
+        if (i == sn.length()) {
+            return 1;
+        }
+        if (!same && dp[i][mask] >= 0) {
+            return dp[i][mask];
+        }
+        int res = 0, t = same ? (sn.charAt(i) - '0') : 9;
+        for (int k = 0; k <= t; k++) {
+            if ((mask & (1 << k)) != 0) {
+                continue;
+            }
+            res += f(mask == 0 && k == 0 ? mask : mask | (1 << k), sn, i + 1, same && k == t);
+        }
+        if (!same) {
+            dp[i][mask] = res;
+        }
+        return res;
+    }
+}
