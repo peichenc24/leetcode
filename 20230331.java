6674f8cf697c999ffe189e5280cad315015066f8
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-03-31 21:30:41 +0800

    Create 20230331.java

---
+++ 20230331.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int ans = 0;
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] - nums[i] != diff) {
+                    continue;
+                }
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[k] - nums[j] == diff) {
+                        ans++;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+}
