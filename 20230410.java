49986bd8a482acd6e9c13f120201bd134babcb78
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-04-10 22:51:34 +0800

    Create 20230410.java

---
+++ 20230410.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+        List<Integer> ans = new ArrayList<Integer>();
+        Deque<int[]> stack = new ArrayDeque<int[]>();
+
+        ListNode cur = head;
+        int idx = -1;
+        while (cur != null) {
+            ++idx;
+            ans.add(0);
+            while (!stack.isEmpty() && stack.peek()[0] < cur.val) {
+                ans.set(stack.pop()[1], cur.val);
+            }
+            stack.push(new int[]{cur.val, idx});
+            cur = cur.next;
+        }
+
+        int size = ans.size();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; ++i) {
+            arr[i] = ans.get(i);
+        }
+        return arr;
+    }
+}
