9b30b8abe0aae06dfc454a36b777e6b6621d4f5b
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-04-13 22:36:44 +0800

    Create 20230413.java

---
+++ 20230413.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mostFrequentEven(int[] nums) {
+        Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+        for (int x : nums) {
+            if (x % 2 == 0) {
+                count.put(x, count.getOrDefault(x, 0) + 1);
+            }
+        }
+        int res = -1, ct = 0;
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            if (res == -1 || entry.getValue() > ct || entry.getValue() == ct && res > entry.getKey()) {
+                res = entry.getKey();
+                ct = entry.getValue();
+            }
+        }
+        return res;
+    }
+}
