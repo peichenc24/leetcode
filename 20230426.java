36c32a96067df4a467ee05bbe98f436b72fa0f61
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-04-26 23:05:32 +0800

    Create 20230426.java

---
+++ 20230426.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxSumTwoNoOverlap(int[] nums, int firstLen, int secondLen) {
+        return Math.max(help(nums, firstLen, secondLen), help(nums, secondLen, firstLen));
+    }
+
+    public int help(int[] nums, int firstLen, int secondLen) {
+        int suml = 0;
+        for (int i = 0; i < firstLen; ++i) {
+            suml += nums[i];
+        }
+        int maxSumL = suml;
+        int sumr = 0;
+        for (int i = firstLen; i < firstLen + secondLen; ++i) {
+            sumr += nums[i];
+        }
+        int res = maxSumL + sumr;
+        for (int i = firstLen + secondLen, j = firstLen; i < nums.length; ++i, ++j) {
+            suml += nums[j] - nums[j - firstLen];
+            maxSumL = Math.max(maxSumL, suml);
+            sumr += nums[i] - nums[i - secondLen];
+            res = Math.max(res, maxSumL + sumr);
+        }
+        return res;
+    }
+}
