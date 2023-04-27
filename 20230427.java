08cd7c352af398a684e3bd9b91b4d52d8cb2f093
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-04-27 23:00:45 +0800

    Create 20230427.java

---
+++ 20230427.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestStrChain(String[] words) {
+        Map<String, Integer> cnt = new HashMap<String, Integer>();
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int res = 0;
+        for (String word : words) {
+            cnt.put(word, 1);
+            for (int i = 0; i < word.length(); i++) {
+                String prev = word.substring(0, i) + word.substring(i + 1);
+                if (cnt.containsKey(prev)) {
+                    cnt.put(word, Math.max(cnt.get(word), cnt.get(prev) + 1));
+                }
+            }
+            res = Math.max(res, cnt.get(word));
+        }
+        return res;
+    }
+}
