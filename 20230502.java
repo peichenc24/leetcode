05339360478f7e4a8053840c338cd6fba793db18
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-05-02 22:25:41 +0800

    Create 20230502.java

---
+++ 20230502.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> set = new HashSet<Integer>();
+        int value1 = 1;
+        for (int i = 0; i < 21; i++) {
+            int value2 = 1;
+            for (int j = 0; j < 21; j++) {
+                int value = value1 + value2;
+                if (value <= bound) {
+                    set.add(value);
+                } else {
+                    break;
+                }
+                value2 *= y;
+            }
+            if (value1 > bound) {
+                break;
+            }
+            value1 *= x;
+        }
+        return new ArrayList<Integer>(set);
+    }
+}
