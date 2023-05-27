6ca833a0a06e87da04067e2e79520c3dd2a8640b
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-05-27 23:01:36 +0800

    Create 20230527.java

---
+++ 20230527.java
@@ -0,0 +1,40 @@
+class Solution {
+    public double[] sampleStats(int[] count) {
+        int n = count.length;
+        int total = Arrays.stream(count).sum();
+        double mean = 0.0;
+        double median = 0.0;
+        int minnum = 256;
+        int maxnum = 0;
+        int mode = 0;
+
+        int left = (total + 1) / 2;
+        int right = (total + 2) / 2;
+        int cnt = 0;
+        int maxfreq = 0;
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += (long) count[i] * i;
+            if (count[i] > maxfreq) {
+                maxfreq = count[i];
+                mode = i;
+            }
+            if (count[i] > 0) {
+                if (minnum == 256) {
+                    minnum = i;
+                }
+                maxnum = i;
+            }
+            if (cnt < right && cnt + count[i] >= right) {
+                median += i;
+            }
+            if (cnt < left && cnt + count[i] >= left) {
+                median += i;
+            }
+            cnt += count[i];
+        }
+        mean = (double) sum / total;
+        median = median / 2.0;
+        return new double[]{minnum, maxnum, mean, median, mode};
+    }
+}
