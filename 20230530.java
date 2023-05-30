e9fe0200703f77e97ae38ae8c796f9bdb8b4f414
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-05-31 01:25:37 +1100

    Create 20230530.java

---
+++ 20230530.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        Set<Integer> toDeleteSet = new HashSet<Integer>();
+        for (int val : to_delete) {
+            toDeleteSet.add(val);
+        }
+        List<TreeNode> roots = new ArrayList<TreeNode>();
+        dfs(root, true, toDeleteSet, roots);
+        return roots;
+    }
+
+    public TreeNode dfs(TreeNode node, boolean isRoot, Set<Integer> toDeleteSet, List<TreeNode> roots) {
+        if (node == null) {
+            return null;
+        }
+        boolean deleted = toDeleteSet.contains(node.val);
+        node.left = dfs(node.left, deleted, toDeleteSet, roots);
+        node.right = dfs(node.right, deleted, toDeleteSet, roots);
+        if (deleted) {
+            return null;
+        } else {
+            if (isRoot) {
+                roots.add(node);
+            }
+            return node;
+        }
+    }
+}
