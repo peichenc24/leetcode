3f2e349d2553e7a7857a58e7df08680b120fabc9
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-06-06 23:28:48 +0800

    Create 20230606.java

---
+++ 20230606.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int res = 0, n = grid.length;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (equal(row, col, n, grid)) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    public boolean equal(int row, int col, int n, int[][] grid) {
+        for (int i = 0; i < n; i++) {
+            if (grid[row][i] != grid[i][col]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
