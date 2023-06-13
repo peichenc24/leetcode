19fa7e197650f63b5b5e7b299815f24c7e9a9631
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-06-13 22:28:08 +0800

    Create 20230613.java

---
+++ 20230613.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int unequalTriplets(int[] nums) {
+        int res = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]) {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
