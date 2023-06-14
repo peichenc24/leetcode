4438e4aaad3727069b52444927f6ea925de7b598
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-06-15 01:57:47 +1100

    Create 20230614.java

---
+++ 20230614.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numTimesAllBlue(int[] flips) {
+        int n = flips.length;
+        int ans = 0, right = 0;
+        for (int i = 0; i < n; ++i) {
+            right = Math.max(right, flips[i]);
+            if (right == i + 1) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+}
