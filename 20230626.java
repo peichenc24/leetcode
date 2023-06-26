972d8c33b9c5fb3643a27b8de839b7890c27ca42
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-06-26 23:11:12 +0800

    Create 20230626.java

---
+++ 20230626.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int t = (n * n + n) / 2;
+        int x = (int) Math.sqrt(t);
+        if (x * x == t) {
+            return x;
+        }
+        return -1;
+    }
+}
