0bb7d2192000b31c634f2255fc6a11b094367354
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-08-03 02:27:20 +1100

    Create 20230802.java

---
+++ 20230802.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int flipgame(int[] fronts, int[] backs) {
+        Set<Integer> same = new HashSet();
+        for (int i = 0; i < fronts.length; ++i) {
+            if (fronts[i] == backs[i]) {
+                same.add(fronts[i]);
+            }
+        }
+        int res = 3000;
+        for (int x : fronts) {
+            if (x < res && !same.contains(x)){
+                res = x;
+            }
+        }
+        for (int x : backs) {
+            if (x < res && !same.contains(x)) {
+                res = x;
+            }
+        }
+        return res % 3000;
+    }
+}
