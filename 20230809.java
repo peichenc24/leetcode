e6b7c03b405e0e4818b5f763573c8b322fa41ff8
Pei-Chen <41612241+peichenc24@users.noreply.github.com>
2023-08-10 01:56:29 +1100

    Create 20230809.java

---
+++ 20230809.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int m = 1, s = 0;
+        while (n != 0) {
+            int x = n % 10;
+            n /= 10;
+            m *= x;
+            s += x;
+        }
+        return m - s;
+    }
+}
